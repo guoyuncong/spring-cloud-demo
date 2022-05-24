bc0e033917ab2b365b51251fa70f1aca124362c5
guoyuncong <guoyuncong@lazbao.com>
2022-05-24 20:29:48 +0800

    feat: 配置屬性映射

--- spring-cloud-nacos-config/src/main/java/my/springcloud/nacos/config/Person.java
+++ spring-cloud-nacos-config/src/main/java/my/springcloud/nacos/config/Person.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -24,8 +25,10 @@ public class Person {
     private Boolean boss;
     private Date birth;
     private Map<String, Object> maps;
+    private Map<String, Dog> dogMaps;
     private List<Object> lists;
     private String[] pets;
+    private Dog[] dogs;
     private Dog dog;
 
     @Override
@@ -36,8 +39,10 @@ public class Person {
                 ", boss=" + boss +
                 ", birth=" + birth +
                 ", maps=" + maps +
+                ", dogMaps=" + dogMaps +
                 ", lists=" + lists +
                 ", pets=" + Arrays.toString(pets) +
+                ", dogs=" + Arrays.toString(dogs) +
                 ", dog=" + dog +
                 '}';
     }
